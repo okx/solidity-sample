a87cdde87853e795e392387982ca583009b50d53
xiangjianmeng <805442788@qq.com>
2022-10-12 11:17:23 +0800

    add web3j erc20 tranferFrom

--- java/exchain-web3-sample/src/main/java/com/exchain/web3/util/ERC20Sample.java
+++ java/exchain-web3-sample/src/main/java/com/exchain/web3/util/ERC20Sample.java
@@ -8,54 +8,71 @@ import org.web3j.protocol.http.HttpService;
 import org.web3j.tx.RawTransactionManager;
 import org.web3j.tx.gas.DefaultGasProvider;
 
+import java.io.IOException;
 import java.math.BigInteger;
 
 
 class Sample {
 
-    static public void main(String[] args) throws Exception {
+    static public ERC20 getERC20Instance(Credentials credentials) throws IOException {
         // Connect Web3j to the Blockchain
         String rpcEndpoint = "https://exchaintestrpc.okex.org";
         Web3j web3j = Web3j.build(new HttpService(rpcEndpoint));
 
-        // Prepare a wallet
-        String pk = "your private key";
-
-        Credentials credentials = Credentials.create(pk);
-
-        String addr = credentials.getAddress();
-        System.out.println("address: " + addr);
-
-        RawTransactionManager txManager = new RawTransactionManager(web3j, credentials, Byte.valueOf(web3j.netVersion().send().getNetVersion()));
+        RawTransactionManager txManager = new RawTransactionManager(web3j, credentials, Byte.parseByte(web3j.netVersion().send().getNetVersion()));
 
         // Load the USDT contract
         String contractAddress = "0xe579156f9decc4134b5e3a30a24ac46bb8b01281";
-        ERC20 erc20 = ERC20.load(contractAddress, web3j, txManager, new DefaultGasProvider());
+        return ERC20.load(contractAddress, web3j, txManager, new DefaultGasProvider());
+    }
 
+    static public void main(String[] args) throws Exception {
+        // prepare private key 1
+        Credentials credential1 = Credentials.create("private key1");
+        String addr1 = credential1.getAddress();
+        System.out.println("address: " + addr1);
 
-        String symbol = erc20.symbol().send();
-        String name = erc20.name().send();
-        BigInteger decimal = erc20.decimals().send();
+        // prepare private key 2
+        Credentials credential2 = Credentials.create("private key2");
+        String addr2 = credential2.getAddress();
+        System.out.println("address: " + addr2);
+
+        ERC20 usdt = getERC20Instance(credential1);
+
+        String symbol = usdt.symbol().send();
+        String name = usdt.name().send();
+        BigInteger decimal = usdt.decimals().send();
 
         System.out.println("symbol: " + symbol);
         System.out.println("name: " + name);
         System.out.println("decimal: " + decimal.intValueExact());
 
         // transfer
-        TransactionReceipt receipt = erc20.transfer("0x0db6b797e64666d4b36b13e5dc6fcd4661893ac8", new BigInteger("25")).send();
-        System.out.println("Transaction hash: "+receipt.getTransactionHash());
+        BigInteger balance1 = usdt.balanceOf(addr1).send();
+        System.out.println("balance (" + addr1 + ")=" + balance1.toString());
 
-        BigInteger balance1 = erc20.balanceOf(addr).send();
-        System.out.println("balance (" + addr + ")="+balance1.toString());
+        TransactionReceipt receipt = usdt.transfer(addr2, new BigInteger("25")).send();
+        System.out.println("Transaction hash: " + receipt.getTransactionHash());
 
-        BigInteger balance2 = erc20.balanceOf("0x0db6b797e64666d4b36b13e5dc6fcd4661893ac8").send();
-        System.out.println("balance (0x0db6b797e64666d4b36b13e5dc6fcd4661893ac8)="+balance2.toString());
+        balance1 = usdt.balanceOf(addr1).send();
+        System.out.println("balance (" + addr1 + ")=" + balance1.toString());
+
+        BigInteger balance2 = usdt.balanceOf(addr2).send();
+        System.out.println("balance (" + addr2 + ")=" + balance2.toString());
 
         // approve
-        TransactionReceipt receipt1 = erc20.approve("0x0db6b797e64666d4b36b13e5dc6fcd4661893ac8", new BigInteger("100000")).send();
-        System.out.println("Transaction hash: "+receipt1.getTransactionHash());
+        TransactionReceipt receipt1 = usdt.approve(addr2, new BigInteger("100000")).send();
+        System.out.println("Transaction hash: " + receipt1.getTransactionHash());
 
-        BigInteger approveAmt = erc20.allowance(addr,"0x0db6b797e64666d4b36b13e5dc6fcd4661893ac8").send();
-        System.out.println("allowance (0x0db6b797e64666d4b36b13e5dc6fcd4661893ac8)="+approveAmt.toString());
+        BigInteger approveAmt = usdt.allowance(addr1, addr2).send();
+        System.out.println("allowance (" + addr2 + ")=" + approveAmt.toString());
+
+        // transferFrom
+        usdt = getERC20Instance(credential2);
+        TransactionReceipt receipt2 = usdt.transferFrom(addr1, "0x0db6b797e64666d4b36b13e5dc6fcd4661893ac8", new BigInteger("50000")).send();
+        System.out.println("Transaction hash: " + receipt2.getTransactionHash());
+
+        approveAmt = usdt.allowance(addr1, addr2).send();
+        System.out.println("allowance (" + addr2 + ")=" + approveAmt.toString());
     }
 }
