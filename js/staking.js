7692932cd3f0cf12322ce1a87cd1743bcd6d91c1
巴扎黑 <1298170866@qq.com>
2022-11-14 14:44:04 +0800

    Merge PR: update contract address (#8)
    Update contract address

--- js/staking.js
+++ js/staking.js
@@ -118,8 +118,11 @@ const userAddress = ""
 const amount = "100"
 
 //contract address in okc test net
-const systemContarctAddress = "0x7e5E6AF6424BE7A835313777Fc6E0d1912e52Fc8"
-const stakingMSGHelperContractAddress = "0xE6947172736e3ed558Ec375cf16C62488627a18e"
+//note: okc main net is: 0xd6bce454316b8ddFb76bB7bb1B57B8942B09Acd5
+const systemContarctAddress = "0x727d14EfC4FB5281A18A6d62BCf486a1cF4d2210"
+//stakingMSGHelper proxy contract address in okc test net
+//note: okc main net is: 0x1b29c875Bd7Ec9a12C29fc6eeF8E451207352EF3
+const stakingMSGHelperContractAddress = "0x37AFc0Fe2b14acC1593C4435F3B53ac0335Ee292"
 
 //init web3, connect okc test net
 const web3 = new Web3(new Web3.providers.HttpProvider("https://exchaintestrpc.okex.org"));
