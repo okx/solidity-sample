690bc1a52b21790bf2dd394f418f1099da0e7651
巴扎黑 <1298170866@qq.com>
2022-12-27 11:42:18 +0800

    Modify misleading comment, add functional use case

--- js/staking.js
+++ js/staking.js
@@ -114,23 +114,26 @@ const stakingMSGHelperABI = [
 const userPrivateKey = ""
 //user address, add you address
 const userAddress = ""
-//the OKT you want deposit
+//validator address
+const validatorAddresses = ""
+const validatorAddresses2 = ""
+//the OKT you want deposit,Minimum amount is 1.(1 = 1 OKT)
 const amount = "100"
 
-//contract address in okc test net
-//note: okc main net is: 0xd6bce454316b8ddFb76bB7bb1B57B8942B09Acd5
-const systemContarctAddress = "0x727d14EfC4FB5281A18A6d62BCf486a1cF4d2210"
-//stakingMSGHelper proxy contract address in okc test net
-//note: okc main net is: 0x1b29c875Bd7Ec9a12C29fc6eeF8E451207352EF3
-const stakingMSGHelperContractAddress = "0x37AFc0Fe2b14acC1593C4435F3B53ac0335Ee292"
+//contract address in okc main net
+const systemContractAddress = "0xd6bce454316b8ddFb76bB7bb1B57B8942B09Acd5"
+//stakingMSGHelper proxy contract address in okc main net
+const stakingMSGHelperContractAddress = "0x1b29c875Bd7Ec9a12C29fc6eeF8E451207352EF3"
 
-//init web3, connect okc test net
-const web3 = new Web3(new Web3.providers.HttpProvider("https://exchaintestrpc.okex.org"));
+
+//init web3, connect okc main net
+const web3 = new Web3(new Web3.providers.HttpProvider("https://exchainrpc.okex.org"));
 
 //init contract object
-const systemContract = new web3.eth.Contract(systemContractABI, systemContarctAddress);
+const systemContract = new web3.eth.Contract(systemContractABI, systemContractAddress);
 const stakingMSGHelperContract = new web3.eth.Contract(stakingMSGHelperABI, stakingMSGHelperContractAddress);
 
+//deposit
 //get data
 let backData = await stakingMSGHelperContract.methods.genDepositMsg(amount).call({from: userAddress})
 
@@ -140,7 +143,58 @@ let encodeData  = await systemContract.methods.invoke(backData).encodeABI();
 //sign
 let sign = await web3.eth.accounts.signTransaction({
     gas: 500000,
-    to: systemContarctAddress,
+    to: systemContractAddress,
+    data: encodeData,
+}, userPrivateKey)
+
+//send tx
+result = await web3.eth.sendSignedTransaction(sign.rawTransaction)
+
+//add validator addresses
+//get data
+backData = await stakingMSGHelperContract.methods.genAddSharesMsg([validatorAddresses, validatorAddresses2]).call({from: userAddress})
+
+//encode
+encodeData  = await systemContract.methods.invoke(backData).encodeABI();
+
+//sign
+sign = await web3.eth.accounts.signTransaction({
+    gas: 500000,
+    to: systemContractAddress,
+    data: encodeData,
+}, userPrivateKey)
+
+//send tx
+result = await web3.eth.sendSignedTransaction(sign.rawTransaction)
+
+//withdraw all rewards
+//get data
+backData = await stakingMSGHelperContract.methods.genWithdrawAllRewardsMsg().call({from: userAddress})
+
+//encode
+encodeData  = await systemContract.methods.invoke(backData).encodeABI();
+
+//sign
+sign = await web3.eth.accounts.signTransaction({
+    gas: 500000,
+    to: systemContractAddress,
+    data: encodeData,
+}, userPrivateKey)
+
+//send tx
+result = await web3.eth.sendSignedTransaction(sign.rawTransaction)
+
+//withdraw
+//get data
+backData = await stakingMSGHelperContract.methods.genWithdrawMsg(amount).call({from: userAddress})
+
+//encode
+encodeData  = await systemContract.methods.invoke(backData).encodeABI();
+
+//sign
+sign = await web3.eth.accounts.signTransaction({
+    gas: 500000,
+    to: systemContractAddress,
     data: encodeData,
 }, userPrivateKey)
 
